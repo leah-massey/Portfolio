cb68cf7c60cf59e2df6db7f9b67fef55257ca594
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-05 12:18:29 +0100

    Basic layout of Navbar, including hamburger, mobile menu and social icons.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="logo image" style={{ width: "50px" }} />
+      </div>
+
+      {/* menu */}
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* hamburger */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      {/* mobile menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* social icons */}
+      <div></div>
+    </div>
+  );
 };
 
 export default Navbar;
