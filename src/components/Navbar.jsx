735a449c874ade3a7e8ae4a6992b4f527f005a91
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-05 13:14:06 +0100

    Padding added to Navbar headings. Mobile, hamburger and social icons hidden.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/logo1.png";
 
 const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="logo image" style={{ width: "50px" }} />
+        <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
       </div>
 
       {/* menu */}
@@ -35,7 +35,7 @@ const Navbar = () => {
       </ul>
 
       {/* social icons */}
-      <div></div>
+      <div className="hidden"></div>
     </div>
   );
 };
