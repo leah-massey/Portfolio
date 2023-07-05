9ac6d29989c8b94c011d6f0033a1b697c1d57b6f
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-05 16:39:05 +0100

    Background image added.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return;
-  // <div className="w-full h-screen bg-[#0a192f]"></div>;
+  return (
+    <div className="flex items-center jsutify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+      {/* Overlay */}
+      <div className="absolute top-0 right-0 bottom-0 bg-black"></div>
+    </div>
+  );
 };
 
 export default Home;
