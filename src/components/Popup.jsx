5bb524b063f469c109b5d8f8ad0f5ad98d052478
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-11 18:14:25 +0100

    Functioning zoom out/in buttons added to CV popup.

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,13 +1,41 @@
 import React from "react";
 import "./Popup.css";
 import CV from "../assets/Leah-CV-single-page.png";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaPlus, FaMinus } from "react-icons/fa";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 function Popup(props) {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
-        <img src={CV} alt="CV" />
+        <TransformWrapper
+          defaultScale={1}
+          defaultPositionX={100}
+          defaultPositionY={200}
+        >
+          {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+            <>
+              <div className="flex items-start">
+                <button
+                  className="bg-cyan-950 group text-gray-50  px-1 py-1 my-2 flex items-center -mt-4 mr-2"
+                  onClick={() => zoomOut()}
+                >
+                  <FaMinus className="px-1.2" />
+                </button>
+                <button
+                  className="bg-cyan-950 group text-gray-50  px-1 py-1 my-2 flex items-center -mt-4"
+                  onClick={() => zoomIn()}
+                >
+                  <FaPlus className="px-1.2" />
+                </button>
+              </div>
+
+              <TransformComponent>
+                <img src={CV} alt="CV" />
+              </TransformComponent>
+            </>
+          )}
+        </TransformWrapper>
         <button className="close-btn" onClick={() => props.setTrigger(false)}>
           <FaTimes />
         </button>
