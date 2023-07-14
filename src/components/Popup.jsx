155060663da1627fac62aee41f94b5bf9f827df2
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-14 11:33:27 +0100

    Popup updates.

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,41 +2,13 @@ import React from "react";
 import "./Popup.css";
 import CV from "../assets/Leah-CV-single-page.png";
 import { FaTimes, FaPlus, FaMinus } from "react-icons/fa";
-// import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 function Popup(props) {
   return props.trigger ? (
-    <div className="popup">
+    <div className="popup zoom">
       <div className="popup-inner">
         <img src={CV} alt="CV" />
-        {/* <TransformWrapper
-          defaultScale={0}
-          defaultPositionX={0}
-          defaultPositionY={0}
-        >
-          {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-            <>
-              <div className="flex items-start">
-                <button
-                  className="bg-cyan-950 group text-gray-50  px-1 py-1 my-2 flex items-center -mt-4 mr-2"
-                  onClick={() => zoomOut()}
-                >
-                  <FaMinus className="px-1.2" />
-                </button>
-                <button
-                  className="bg-cyan-950 group text-gray-50  px-1 py-1 my-2 flex items-center -mt-4"
-                  onClick={() => zoomIn()}
-                >
-                  <FaPlus className="px-1.2" />
-                </button>
-              </div>
 
-              <TransformComponent>
-                <img src={CV} alt="CV" className="cv-image" />
-              </TransformComponent>
-            </>
-          )}
-        </TransformWrapper> */}
         <button className="close-btn" onClick={() => props.setTrigger(false)}>
           <FaTimes />
         </button>
