0f717467ab4e91043caae044726a88f866097466
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-14 12:22:44 +0100

    Footer added.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="py-6 ">
+      <p className="text-center">
+        This site was designed and built by me and is hosted by Netlify.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
