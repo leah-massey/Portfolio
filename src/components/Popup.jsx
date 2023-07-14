735fdb54a761da85b4b427f4db759344b398c452
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-14 12:11:40 +0100

    Tinkering with popup, no changes of note.

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Popup.css";
 import CV from "../assets/Leah-CV-single-page.png";
-import { FaTimes, FaPlus, FaMinus } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 function Popup(props) {
   return props.trigger ? (
-    <div className="popup zoom">
+    <div className="popup">
       <div className="popup-inner">
         <img src={CV} alt="CV" />
-
         <button className="close-btn" onClick={() => props.setTrigger(false)}>
           <FaTimes />
         </button>
